3c8d710e1e8c302eafe7762152d7881a97f259bc
Arthur Vinicios <arthurogomes@hotmail.com>
2024-06-12 11:27:13 -0300

    Rendering pieces on the board

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,12 +1,16 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
-    private Board board;
+    private final Board board;
 
     public ChessMatch() {
         board = new Board(8, 8);
+        initialSetup();
     }
 
     public Board getBoard() {
@@ -24,5 +28,10 @@ public class ChessMatch {
         }
 
         return chessPieces;
+    }
+
+    private void initialSetup() {
+        board.placePiece(new Rook(board, Color.BLACK), new Position(2, 1));
+        board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
     }
 }
