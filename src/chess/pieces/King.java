6e6e714f4acd598c41526a79ec5b7bfdbccb987e
Arthur Vinicios <arthurogomes@hotmail.com>
2024-06-18 13:39:18 -0300

    King piece movement

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -15,8 +16,65 @@ public class King extends ChessPiece {
         return "K";
     }
 
+    private boolean canMove(Position position) {
+        ChessPiece piece = (ChessPiece) getBoard().piece(position);
+        return piece == null || piece.getColor() != getColor();
+    }
+
     @Override
     public boolean[][] possibleMoves() {
-        return new boolean[0][];
+        boolean[][] grid = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position pos = new Position(0, 0);
+
+        // up
+        pos.setValues(position.getRow() - 1, position.getColumn());
+        if (getBoard().positionExists(pos) && canMove(pos)) {
+            grid[pos.getRow()][pos.getColumn()] = true;
+        }
+
+        // down
+        pos.setValues(position.getRow() + 1, position.getColumn());
+        if (getBoard().positionExists(pos) && canMove(pos)) {
+            grid[pos.getRow()][pos.getColumn()] = true;
+        }
+
+        // left
+        pos.setValues(position.getRow(), position.getColumn() - 1);
+        if (getBoard().positionExists(pos) && canMove(pos)) {
+            grid[pos.getRow()][pos.getColumn()] = true;
+        }
+
+        // right
+        pos.setValues(position.getRow(), position.getColumn() + 1);
+        if (getBoard().positionExists(pos) && canMove(pos)) {
+            grid[pos.getRow()][pos.getColumn()] = true;
+        }
+
+        // nw
+        pos.setValues(position.getRow() - 1, position.getColumn() - 1);
+        if (getBoard().positionExists(pos) && canMove(pos)) {
+            grid[pos.getRow()][pos.getColumn()] = true;
+        }
+
+        // ne
+        pos.setValues(position.getRow() - 1, position.getColumn() + 1);
+        if (getBoard().positionExists(pos) && canMove(pos)) {
+            grid[pos.getRow()][pos.getColumn()] = true;
+        }
+
+        // sw
+        pos.setValues(position.getRow() + 1, position.getColumn() - 1);
+        if (getBoard().positionExists(pos) && canMove(pos)) {
+            grid[pos.getRow()][pos.getColumn()] = true;
+        }
+
+        // se
+        pos.setValues(position.getRow() + 1, position.getColumn() + 1);
+        if (getBoard().positionExists(pos) && canMove(pos)) {
+            grid[pos.getRow()][pos.getColumn()] = true;
+        }
+
+        return grid;
     }
 }
